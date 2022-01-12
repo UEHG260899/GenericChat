87c5483012fb74f9f7fd4cf8249059d97a2de5cb
Uriel Enrique <urielenrique99@gmail.com>
2022-01-11 21:31:17 -0600

    basic project structure

---
+++ GenericChat/Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VideoPlayerViewController.swift
+//  GenericChat
+//
+//  Created by Uriel Hernandez Gonzalez on 11/01/22.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
