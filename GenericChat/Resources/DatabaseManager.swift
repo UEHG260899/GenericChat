80ef9085699eb4dd9c6b2b84632f3bb387d92377
Uriel Enrique <urielenrique99@gmail.com>
2022-01-14 21:23:40 -0600

    fixed issue with email keys in Firebase

--- GenericChat/Resources/DatabaseManager.swift
+++ GenericChat/Resources/DatabaseManager.swift
@@ -21,19 +21,20 @@ extension DatabaseManager {
     
     /// Validate if email exists in database
     public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
-        database.child(email).observeSingleEvent(of: .value) { snapshot in
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        database.child(safeEmail).observeSingleEvent(of: .value) { snapshot in
             guard let _ = snapshot.value as? String else {
                 completion(false)
                 return
             }
-            
             completion(true)
         }
     }
     
     /// Inserts new user to databse
     public func insertUser(with user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name" : user.firstName,
             "last_name" : user.lastName
         ])
@@ -44,4 +45,9 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    var safeEmail: String {
+        var safeEmail = self.emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
 }
